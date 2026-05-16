9dcf3b5be8c4d04841f61f3cb76ece355bafb363
rjbnc6pgcm-dot <s103122@sses.cy.edu.tw>
2026-05-16 21:40:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
   base: '/pcg-2.0/',
@@ -9,9 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  build: {
+    outDir: 'dist',
+  }
 });
